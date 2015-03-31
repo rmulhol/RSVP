a7390c0308af9a74c9f57352be5f2a2a593e792f
Robert Mulholand <robmulholand@gmail.com>
2015-03-31 11:43:05 -0500

    add log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,5 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "foobar" }
     end
     assert_template "static_pages/home"
+    assert is_logged_in?
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -60,4 +60,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "fobar"
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
